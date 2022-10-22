32643b67272ca8f413593974dd0342228c5537c8
Mahfuj Mahtab Mohot <mohotmohot1@gmail.com>
2022-10-22 13:45:35 -0400

    database model done

--- qna/qna/settings.py
+++ qna/qna/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "home",
     'questions',
     'answer',
+    'category',
 ]
 
 MIDDLEWARE = [
--- qna/questions/models.py
+++ qna/questions/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from home.models import *
+from category.models import *
 # Create your models here.
 class Questions(models.Model):
     Q_Id =models.IntegerField(primary_key =True)
     title = models.CharField(max_length = 30)
     details = models.CharField(max_length = 50)
     U_ID = models.ForeignKey(OurUser,on_delete = models.CASCADE)
-    cat_name = models.CharField(max_length = 50)
+    cat_name = models.ForeignKey(Category,null = False,on_delete=models.CASCADE)
     
